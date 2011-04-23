e2d9504ec83ff89920cbba4a611b4ed5df689ee9
xdite <xuite.joke@gmail.com>
2011-04-23 12:01:33 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Q17::Application.config.secret_token = 'bad457c8397ed7ccd8cf0a9ee19c5075c53a558c436ef056b5b702cadb7aafe9226cac38bf33f72a3bb9daf8cea0f3d6c0f2a1998862d878a839ad318fd29624'
