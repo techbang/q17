f9ec9247c3312dad9f9297e0be79cf7a3886a5f4
xdite <xuite.joke@gmail.com>
2011-05-01 02:15:13 +0800

    實作插入 time_entry , follow ask follow topic

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -3,8 +3,10 @@ class Followship < ActiveRecord::Base
   belongs_to :target , :polymorphic => true
   
   after_create :insert_create_time_entry
-  before_destroy :insert_delete_time_entry
-  #TODO delete 有 bug 沒有 log
+  
+  #after_destroy :insert_delete_time_entry
+  # TODO delete 有 bug 沒有 log
+  # http://whilefalse.net/2011/04/05/rails-3-counter-cache-has_many-through/
   
   protected
   
@@ -17,15 +19,6 @@ class Followship < ActiveRecord::Base
     time_entry.action = "ADD"
     time_entry.save!
   end
-  
-  def insert_delete_time_entry
-    time_entry = self.user.time_entries.build
-    time_entry.resource_type = "Followship"
-    time_entry.resource_id = self.id
-    time_entry.target_type = self.target_type
-    time_entry.target_id = self.target_id
-    time_entry.action = "DELETE"
-    time_entry.save!
-  end
+
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,23 @@ class User < Techbang::User
 
   has_many :followships,:foreign_key => 'user_id',:class_name => 'Followship'
   has_many :befollowships,:foreign_key => 'follower_id',:class_name => 'Followship'
-  
+
   has_many :following_users   ,:through => :followships, :source =>  :followshipped
   has_many :followers,:through => :befollowships, :source => :befollowshipped
-  
-  
+
+
   has_many :followed_ask_ships, :class_name => "Followship"
-  has_many :followed_asks , :through => :followed_ask_ships, :source => :target , :source_type => "Ask"
-  
+  has_many :followed_asks , :through => :followed_ask_ships, :source => :target , :source_type => "Ask" , :before_remove => :insert_unfollow_ask_time_entry
+
   has_many :followed_topic_ships, :class_name => "Followship"
-  has_many :followed_topics , :through => :followed_topic_ships, :source => :target , :source_type => "Topic"
-  
+  has_many :followed_topics , :through => :followed_topic_ships, :source => :target , :source_type => "Topic" , :before_remove => :insert_unfollow_topic_time_entry
+
   has_many :answered_ask_ships
   has_many :answered_asks, :through => :answered_ask_ships, :source => :ask
-  
+
   has_many :muted_ask_ships
   has_many :muted_asks, :through => :muted_ask_ships, :source => :ask
-  
+
   has_many :answers
   has_many :logs
   has_many :time_entries, :foreign_key => "creator_id"
@@ -31,7 +31,7 @@ class User < Techbang::User
   def ask_followed?(ask)
     # Rails.logger.info { "user: #{self.inspect}" }
     # Rails.logger.info { "asks: #{self.followed_asks.inspect}" }
-    # Rails.logger.info { "ask: #{ask.inspect}" } 
+    # Rails.logger.info { "ask: #{ask.inspect}" }
     self.followed_asks.include?(ask)
   end
 
@@ -39,7 +39,7 @@ class User < Techbang::User
   def ask_muted?(ask_id)
     self.muted_ask_ids.include?(ask_id)
   end
-  
+
   def suggest_items
     return UserSuggestItem.gets(self.id, :limit => 6)
   end
@@ -47,85 +47,102 @@ class User < Techbang::User
   def topic_followed?(topic)
     self.followed_topics.include?(topic)
   end
-  
+
   def mute_ask(ask)
     self.muted_asks << ask
   end
-  
+
   def unmute_ask(ask)
     self.muted_asks.delete(ask)
   end
-  
+
   def follow_ask(ask)
     ask.followers << self
     insert_follow_log("FOLLOW_ASK", ask)
   end
-  
+
   def unfollow_ask(ask)
     self.followed_asks.delete(ask)
     insert_follow_log("UNFOLLOW_ASK", ask)
   end
-  
+
   def follow_topic(topic)
     topic.followers << self
-    #return if topic.follower_ids.include? self.id
-    #topic.followers << self
-    #topic.followers_count_changed = true
-    #topic.save
-    #
+
     ## 清除推荐话题
     #UserSuggestItem.delete(self.id, "Topic", topic.id)
-    
+
     #insert_follow_log("FOLLOW_TOPIC", topic)
   end
-  
+
   def unfollow_topic(topic)
     self.followed_topics.delete(topic)
-    #self.save
-    #
-    #topic.followers.delete(self)
-    #topic.followers_count_changed = true
-    #topic.save
-    #
+
     #insert_follow_log("UNFOLLOW_TOPIC", topic)
   end
-  
-  
-  #protected
-  
-    def insert_follow_log(action, item, parent_item = nil)
-      #begin
-      
-        
-        log = self.logs.build
 
-        log.type = "UserLog"
-        log.resource_id = item.id
-        log.resource_type = item.class.to_s
-        log.title = self.name
-        log.action = action
-        log.diff = ""
-        log.save
-        
-       #log = UserLog.new
-       #log.user_id = self.id
-       #log.title = self.name
-       #log.target_id = item.id
-       #log.action = action
-       #if parent_item.blank?
-       #  log.target_parent_id = item.id
-       #  log.target_parent_title = item.is_a?(Ask) ? item.title : item.name
-       #else
-       #  log.target_parent_id = parent_item.id
-       #  log.target_parent_title = parent_item.title
-       #end
-       #log.diff = ""
-       #log.save
-      #rescue Exception => e
-        
-      #end
+
+
+  #protected
+
+  def insert_follow_log(action, item, parent_item = nil)
+    #begin
+
+
+    log = self.logs.build
+
+    log.type = "UserLog"
+    log.resource_id = item.id
+    log.resource_type = item.class.to_s
+    log.title = self.name
+    log.action = action
+    log.diff = ""
+    log.save
+
+    #log = UserLog.new
+    #log.user_id = self.id
+    #log.title = self.name
+    #log.target_id = item.id
+    #log.action = action
+    #if parent_item.blank?
+    #  log.target_parent_id = item.id
+    #  log.target_parent_title = item.is_a?(Ask) ? item.title : item.name
+    #else
+    #  log.target_parent_id = parent_item.id
+    #  log.target_parent_title = parent_item.title
+    #end
+    #log.diff = ""
+    #log.save
+    #rescue Exception => e
+
+    #end
+  end
+
+  private
+  
+    # 該死的 before_destroy 不起作用只好用這種方式解
+  
+    def insert_unfollow_ask_time_entry(record)
+      time_entry = self.time_entries.build
+      ship = Followship.find_by_user_id_and_target_type_and_target_id(self,  "Ask" , record.id)
+      time_entry.resource_type = "Followship"
+      time_entry.resource_id = ship.id
+      time_entry.target_type = "Ask"
+      time_entry.target_id = record.id
+      time_entry.action = "DELETE"
+      time_entry.save!
     end
-    
+
+    def insert_unfollow_topic_time_entry(record)
+      time_entry = self.time_entries.build
+      ship = Followship.find_by_user_id_and_target_type_and_target_id(self,  "ActsAsTaggableOn::Tag" , record.id)
+      time_entry.resource_type = "Followship"
+      time_entry.resource_id = ship.id
+      time_entry.target_type = "Topic"
+      time_entry.target_id = record.id
+      time_entry.action = "DELETE"
+      time_entry.save!
+    end
 end
 
 # == Schema Information
